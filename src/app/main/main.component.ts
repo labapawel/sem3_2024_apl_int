e4e6865d1d4709105bee2f9defb0c8695fc06b7d
Paweł Łaba <labapawel@gmail.com>
2024-10-12 09:30:11 +0200

    zmiany z 12-10-2024 dodanie routingu, serwisów i interfejsów

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { WorkingService } from '../working.service';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  constructor(public working : WorkingService){
+    working.sub().subscribe( tasks => {
+      console.log(tasks);
+      
+    })
+  }
+
+}
